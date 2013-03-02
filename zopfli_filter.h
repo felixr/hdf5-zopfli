ffc3882465ed119692a83ed99a67453352afd322
Felix Riedel <felix.riedel@gmail.com>
2013-03-02 09:35:02 +0100

    replaced Filter ID

--- zopfli_filter.h
+++ zopfli_filter.h
@@ -1,17 +1,14 @@
 #ifndef H5PY_ZOPFLI_H
 #define H5PY_ZOPFLI_H
 
-/* Filter revision number, starting at 1 */
-#define H5_FILTER_ZOPFLI_VERSION 1
-
-/* Filter ID registered with the HDF Group as of 2/6/09.  For maintenance
-   requests, contact the filter author directly. */
-#define H5_FILTER_ZOPFLI 2600 
+/* Filter ID from the testing range (-> not for production use)
+ * http://www.hdfgroup.org/HDF5/doc/H5.user/Filters.html
+ */ 
+#define H5_FILTER_ZOPFLI 256 
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-
 
 /* Register the filter with the library. Returns a negative value on failure, 
    and a non-negative value on success.
